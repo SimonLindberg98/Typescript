b589c245278db0753f64c233d201ccabf3ce8b8d
SimonLindberg98 <simonlindberg98@gmail.com>
2016-04-06 15:01:37 +0200

    Working sprites
    Learned how to use and animate with spritesheets

--- Typescript/Donkey Mong/CanvasLibApps/CanvasLibApps/app.js
+++ Typescript/Donkey Mong/CanvasLibApps/CanvasLibApps/app.js
@@ -1,2 +1,13 @@
 //Donkey Mong (totally not ripoff)
+var Player = (function () {
+    function Player() {
+    }
+    return Player;
+})();
+var runChar = new Spritesheet("sick-spritesheet.png", 38, 50);
+var runChar2 = new Animation(runChar, 5, 1, 8);
+update = function () {
+    runChar2.drawFrame(50, 50);
+    runChar2.updateFrame();
+};
 //# sourceMappingURL=app.js.map
--- Typescript/Donkey Mong/CanvasLibApps/CanvasLibApps/app.ts
+++ Typescript/Donkey Mong/CanvasLibApps/CanvasLibApps/app.ts
@@ -1,1 +1,19 @@
 ﻿//Donkey Mong (totally not ripoff)
+
+class Player {
+    public x: number;
+    public y: number;
+    public speed: number;
+    public playerHB: Hitbox;
+
+
+}
+
+var runChar = new Spritesheet("sick-spritesheet.png", 38, 50);
+
+var runChar2 = new Animation(runChar, 5, 1, 8);
+
+update = function () {
+    runChar2.drawFrame(50, 50);
+    runChar2.updateFrame();
+}
